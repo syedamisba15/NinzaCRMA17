52b672c30ba06087785f58772a664393a90eb906
user <user@DESKTOP-HP54R4O>
2025-11-20 15:15:08 +0530

    committed

---
+++ src/test/java/Practice1/Demo1Test.java
@@ -0,0 +1,12 @@
+package Practice1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test(groups = "smoke")
+	public void AppleTest() {
+		Reporter.log("Apple",true);
+	}
+}
